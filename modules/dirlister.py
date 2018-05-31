59bf980676cfbf86bad1490965bb3bbec1fe386d
123nobody123 <no.body@papierkorb.me>
2018-05-31 05:16:42 -0400

    Adding new modules.

---
+++ modules/dirlister.py
@@ -0,0 +1,6 @@
+import os
+
+def run(**args):
+
+	print "[*] In dirlister module."
+	return str(os.listdir("."))
---
+++ modules/enviroment.py
@@ -0,0 +1,6 @@
+import os
+
+def run(**args):
+
+	print "[*] In enviroment mosule."
+	return str(os.environ)
